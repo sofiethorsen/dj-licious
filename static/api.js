fe0809ac1958b0ad78b7f7c05bd5b72978d57e07
Johan Brodin <johan.brodin@gmai.com>
2013-01-19 21:15:51 +0100

    Added some debug

--- static/api.js
+++ static/api.js
@@ -4,6 +4,7 @@ var _lastestupdate = "";
 
 function callAPI(api_function, callback_function) {
 	request = "http://bok.alexanderheldt.se:8080/api/"+api_function;
+	console.log("Called function: " + api_function);
 	$.getJSON(request, function(data){
 		console.log(data);
 		callback_function(data);
