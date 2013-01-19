06732b587ab821fe952399da156723986aee459b
Johan Brodin <johan.brodin@gmai.com>
2013-01-19 18:17:16 +0100

    Working on API

--- static/api.js
+++ static/api.js
@@ -1,13 +1,16 @@
 
 var _lastestupdate = "";
-var _playlistId = 1;
+var _playlistId = "50fad3edcf1e8c46163c0d7d";
+
+
+//http://bok.alexanderheldt.se:8080/api/
 
 //TODO: Remove this
 var user_id = "1337";
 
 
 function callAPI(api_function, callback_function) {
-	request = "http://bok.alexanderheldt.se:8080/api/"+api_function + "/";
+	request = "http://bok.alexanderheldt.se:8080/api/"+api_function;
 	console.log("Running the request: " + request)
 	$.getJSON(request, function(data){
 		console.log("Reply from server.");
@@ -22,14 +25,14 @@ function checkServer(){
 	//Check if the current timestamp is newer then _lastestupdate
   	console.log("Checking database for changes...");
 
-	//callAPI("test", onTestResult);
+	getPlaylist();
 }
 
-function onTestResult(data) {
-	console.log("test");
-	console.log(data)
-}
 
+
+function getPlaylist() {
+	callAPI("get-playlist/?playlist_id="+_playlistId, console.log);
+}
 
 // Vote for a track
 function vote(track_id) {
@@ -39,5 +42,4 @@ function vote(track_id) {
 
 function addSong(track_id) {
 	//API -> bok.alexanderheldt.se:8080/api/add-song/<playlist_id>/<track_id>
-
 }
