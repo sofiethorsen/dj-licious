82bcb2f964017988686b0b801db8b4de2039ea23
Johan Brodin <johan.brodin@gmai.com>
2013-01-19 22:37:17 +0100

    votefix

--- static/api.js
+++ static/api.js
@@ -18,8 +18,16 @@ function checkServer(){
 
 
 function vote(href, vote) {
-
+	//API bok.alexanderheldt.se:8080/api/vote/<playlist_id>/<track_id>/<facebook_id>/<vote>
+	callAPI("vote/?playlist_id="+_playlistId+"&uri="+href+"facebook_id="+FB.user.id+"&vote="+vote, onVoteResult);
 }
+
+function onVoteResult(data){
+	console.log(data);
+}
+
+
+http://bok.alexanderheldt.se:8080/api/
 
 
 function addSong(href) {
@@ -32,7 +40,7 @@ function onResultAddsong(data){
 }
 
 function removeSong(href) {
-    
+   
 }
 
 
@@ -41,7 +49,15 @@ function getPlaylist() {
 }
 
 function onResultPlaylist(data) {
-	var imgSrc = "https://graph.facebook.com/"+data.result.currently_playing.adder+"/picture?width=100&height=100";
+	var imgSrc = "";
+
+	if (data.result.currently_playing.adder == "") {
+		imgSrc = "http://www.landinst.com/images/placeholder-100x100.png";
+	}
+	else {
+		imgSrc = "https://graph.facebook.com/"+data.result.currently_playing.adder+"/picture?width=100&height=100";
+	}
+
     $("#mainImgContainer").html('<img style="width: 100px; height: 100px" src="'+imgSrc+'"/>');
 	$("#mainTextContainer").html(data.result.currently_playing.track + " </br> " + data.result.currently_playing.artist);
 	$("#queueList").html(generateQueue(data));
